09d7786ca74f9723bb6c4c238d220616a17166f4
juanlopnz <juan.lopnz@uao.edu.co>
2023-02-28 00:23:36 -0500

    Este es el challenge 6

---
+++ practica-personal/src/hooks/useFetch.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+
+export const useFetch = ( url ) => {
+
+    const [estado, setEstado] = useState({
+        data: null,
+        isLoading: true,
+        hasError: null
+    })
+
+    const getFetch = async ( url ) => {
+        const api = await fetch( url )
+        const data = await api.json()
+
+        setEstado({
+            data,
+            isLoading: false,
+            hasError: null
+        })
+
+    }
+
+    useEffect(() => {
+        getFetch()
+    }, [url])
+    
+    return {
+        ...estado
+    }
+}
