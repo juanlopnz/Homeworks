1b610fbefc046eb83347f38ffc4f8d1ff52e1ba1
juanlopnz <jucalora17@gmail.com>
2023-04-19 04:12:09 -0500

    Este es el challenge-13

--- practica-personal/src/App.jsx
+++ practica-personal/src/App.jsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from 'react';
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment, incrementBy } from './store/slices/counterSlices';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { counter } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+
+  const [value, setValue] = useState("");
+
+  const handleInput = (e) => {
+    setValue(e.target.value)
+    console.log(value)
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen bg-purple-50 flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold mb-4">Contador</h1>
+      <div className="bg-white p-6 rounded-lg shadow-lg flex flex-col items-center">
+        <span className="text-5xl font-bold mb-4">{counter}</span>
+        <div className="flex flex-row justify-center items-center mb-4">
+          <button
+            className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded-full mr-2"
+            onClick={() => dispatch(decrement())}
+          >
+            -
+          </button>
+          <button
+            className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded-full"
+            onClick={() => dispatch(increment())}
+          >
+            +
+          </button>
+        </div>
+        <div className="flex flex-row justify-center items-center mb-4">
+          <input type="number" className="py-2 px-4 rounded-full border-purple-500 border-2 mr-2" onChange={handleInput} />
+          <button
+            className="bg-purple-500 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded-full"
+            onClick={() => dispatch(incrementBy(value))}
+          >
+            Agregar
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
--- practica-personal/src/main.jsx
+++ practica-personal/src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { MainApp } from "./pages/MainApp";
-import { NavComponent } from "./components/NavComponent";
+import ReactDOM from 'react-dom/client';
 import './index.css'
-import { AppRoutes } from "./components/AppRoutes";
-import { BrowserRouter } from "react-router-dom";
+import App from './App'
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <AppRoutes></AppRoutes>
-  </BrowserRouter>
+  <Provider store={ store }>
+    <App></App>
+  </Provider>
 )
